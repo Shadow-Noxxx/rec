64b88f3161563c8b533199943f7994f0e580f4a4
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-09 21:57:24 +0530

    Update recover.py

--- recover.py
+++ recover.py
@@ -1,9 +1,12 @@
 from pyrogram import Client
 
-# Your Telegram API credentials and session string
+# Your credentials
 api_id = 23212132
 api_hash = "1c17efa86bdef8f806ed70e81b473c20"
-session_string = "BQFiMGQAhf4JrlOx0d230td4NFd5nlerbkQkVEMJIpYR974l2sOL8883iFRY_ijV_taYcsF9D_y6GdxpFad90Wo6QjUPuqMfVvlvS3nckX_mwz0LtJ2hK2i1yAabE4WOD9QnFiT4hp4NfhqQrxpX4zlEziRdBwWft"
+session_string = (
+    "BQFiMGQAhf4JrlOx0d230td4NFd5nlerbkQkVEMJIpYR974l2sOL8883iFRY_ijV_taYcsF9D_y6GdxpFad90Wo6QjUPuqMfVvlvS3nckX_mwz0LtJ2hK2i1yAabE4WOD9QnFiT4hp4NfhqQrxpX4zlEziRdBwWft"
+    "-O3WE4kWw-6uxiGSrUAVoqOJKUXOZEMwu8PNshf4MPiaPZuPywlkBLa4nexI1vjqtQwX0w48XZVX3eXIHPlREw-xfH3ojJk6Z8rkSWj7rmRL7LGckpTTu3NQiQYZ8aocMYQylxiwyyuf1WqGHgzcXS6PXyV4qzZlZBMxEZLIZk9PYDC_WMj2pqn-wD-TgAAAAHQsVyNAA"
+)
 
 app = Client("fetch_telegram_code", api_id=api_id, api_hash=api_hash, session_string=session_string)
 
